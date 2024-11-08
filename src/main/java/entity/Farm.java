cbbe1aeb678a6239898cd5e19590b9f489032316
punnpunnpunn <86233160+punnpunnpunn@users.noreply.github.com>
2024-11-07 23:24:56 -0500

    Implemented Farm

--- src/main/java/entity/Farm.java
+++ src/main/java/entity/Farm.java
@@ -1,4 +1,34 @@
 package main.java.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Farm {
+
+    // Constructor
+    public Farm(Land farmLand) {
+        this.farmLand = farmLand;
+        this.barnBucks = 0;
+        List<FarmTool> farmTools = new ArrayList<>(FarmTool);
+    }
+
+    public Land getFarmLand() {
+        return this.farmLand;
+    }
+
+    public int getBarnBucks() {
+        return this.barnBucks;
+    }
+
+    public void setBarnBucks(int barnBucks) {
+        this.barnBucks = barnBucks;
+    }
+
+    public ArrayList<FarmTool> getFarmTools() {
+        return this.farmTools;
+    }
+
+    public void addFarmTools(ArrayList<FarmTool> farmTools) {
+        this.farmTools.addAll(farmTools);
+    }
 }
