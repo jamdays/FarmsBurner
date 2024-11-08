df9d60255a76960d0329b7c49d9aad02a12ab9ae
mxiimy <momelody33@gmail.com>
2024-11-07 22:56:18 -0500

    Built constructor, getters, and setters for Farm

--- src/main/java/entity/Farm.java
+++ src/main/java/entity/Farm.java
@@ -1,4 +1,75 @@
 package main.java.entity;
+import java.util.List;
+
+/*
+int size
+List<Crops> crops
+boolean isDry
+boolean isWet
+boolean isSnowy
+ */
 
 public class Farm {
+
+    // instance variables
+    private int size;
+    private List<Crop> crops;
+    private boolean isDry;
+    private boolean isWet;
+    private boolean isSnowy;
+
+    // constructor
+    public Farm(int size, List<Crop> crops) {
+        this.size = size;
+        this.crops = crops;
+        this.isDry = true;
+        this.isWet = false;
+        this.isSnowy = false;
+    }
+
+    // getter and setter for size
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    // getter and setter for crops
+    public List<Crop> getCrops() {
+        return crops;
+    }
+
+    public void setCrops(List<Crop> crops) {
+        this.crops = crops;
+    }
+
+    // getter and setter for isDry
+    public boolean getIsDry() {
+        return isDry;
+    }
+
+    public void setIsDry(boolean isDry) {
+        this.isDry = isDry;
+    }
+
+    // getter and setter for isWet
+    public boolean getIsWet() {
+        return isWet;
+    }
+
+    public void setIsWet(boolean isWet) {
+        this.isWet = isWet;
+    }
+
+    // getter and setter for isSnowy
+
+    public boolean getIsSnowy() {
+        return isSnowy;
+    }
+
+    public void setIsSnowy(boolean isSnowy) {
+        this.isSnowy = isSnowy;
+    }
 }
