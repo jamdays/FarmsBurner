11f71da65fd3dd9962b74e5d22b4010b2b22b040
jamdays <venuedollar@protonmail.com>
2024-11-10 12:13:12 -0500

    fixed plant crop use case based on tile system

--- src/main/java/use_case/plant/PlantOutputBoundary.java
+++ src/main/java/use_case/plant/PlantOutputBoundary.java
@@ -3,6 +3,8 @@ package main.java.use_case.plant;
 public interface PlantOutputBoundary {
     /**
      * adds the crop
+     * @param r, row to be placed at
+     * @param c, column to be placed at
      */
-    void addCrop();
+    void addCrop(int r, int c);
 }
