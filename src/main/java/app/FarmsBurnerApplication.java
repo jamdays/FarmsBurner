19a9086008d23a5b2dd00e0228ef54e743bf69fb
jamdays <venuedollar@protonmail.com>
2024-11-10 23:02:01 -0500

    Api key in main

--- src/main/java/app/FarmsBurnerApplication.java
+++ src/main/java/app/FarmsBurnerApplication.java
@@ -2,14 +2,28 @@ package main.java.app;
 
 import main.java.data_access.OpenWeatherAccess;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class FarmsBurnerApplication {
     public static void main(String[] args) {
         //TODO MAKE A GENERAL DATA ACCESS FOR TESTING ETC
-        final OpenWeatherAccess dao = new OpenWeatherAccess(System.getenv("WAK"));
+        var props = new Properties();
+        var envFile = Paths.get(".env");
+        try (var inputStream = Files.newInputStream(envFile)) {
+            props.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String apiKey = props.get("WAK").toString();
+        final OpenWeatherAccess dao = new OpenWeatherAccess(apiKey);
 
         final AppBuilder builder = new AppBuilder();
         builder.setFarmDAO(dao)
                 .addFarmView()
                 .addPlantUseCase().build().setVisible(true);
+        System.out.println(dao.getWeatherForCity("Toronto"));
     }
 }
