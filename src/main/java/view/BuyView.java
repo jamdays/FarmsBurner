37e71d33b006d177d7ae9080ed9da38c290bb987
mxiimy <momelody33@gmail.com>
2024-11-16 17:22:11 -0500

    Added sprinkler and temporary holders for other items onto buy menu. the plus and minus buttons also work for quantity. we need to decide on prices and what else goes on there.

--- src/main/java/view/BuyView.java
+++ src/main/java/view/BuyView.java
@@ -2,8 +2,8 @@ package main.java.view;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class BuyView {
     public static void main(String[] args) {
@@ -27,23 +27,125 @@ public class BuyView {
         topPanel.add(topRightPanel, BorderLayout.EAST);
         topPanel.setBackground(new java.awt.Color(169, 152, 126));
 
-        // White Background
-        JPanel whitePanel = new JPanel(new BorderLayout());
-        whitePanel.add(topPanel, BorderLayout.NORTH);
-        whitePanel.setBackground(Color.WHITE);
-        whitePanel.setBorder(BorderFactory.createEmptyBorder(320, 700, 20, 20));
+        // Item Panel for Sprinkler
+        // TODO: how much area does the sprinkler water?
+        JPanel itemPanel1 = createItemPanel("Sprinkler", "0", 1, "Automatically waters crops in 1 2x2 area");
 
-        // Main Panel
-        JPanel mainPanel = new JPanel();
-        mainPanel.add(topPanel, BorderLayout.NORTH);
-        mainPanel.add(whitePanel, BorderLayout.CENTER);
+        // TODO: figure out what else goes on the buy menu
+        JPanel itemPanel2 = createItemPanel("item 2", "0", 1, "item 2 description");
+        JPanel itemPanel3 = createItemPanel("item 3", "0", 1, "item 3 description");
+        JPanel itemPanel4 = createItemPanel("item 4", "0", 1, "item 4 description");
+
+        // Main Panel (White Background)
+        JPanel mainPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.NORTH;
+        mainPanel.add(itemPanel1, gbc);
+        gbc.gridy = 1;
+        mainPanel.add(itemPanel2, gbc);
+        gbc.gridy = 2;
+        mainPanel.add(itemPanel3, gbc);
+        gbc.gridy = 3;
+        mainPanel.add(itemPanel4, gbc);
+        mainPanel.setBackground(Color.WHITE);
+
+        // Background Panel (Brown)
+        JPanel backgroundPanel = new JPanel();
+        backgroundPanel.add(topPanel, BorderLayout.NORTH);
+        backgroundPanel.add(mainPanel, BorderLayout.CENTER);
 
         // Frame
         JFrame frame = new JFrame("Buy Menu");
         frame.setSize(741, 420);
-        frame.setContentPane(mainPanel);
+        frame.setContentPane(backgroundPanel);
         frame.getContentPane().setBackground(new java.awt.Color(169, 152, 126));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+    }
+
+    // TODO: implement updateBarnBucks so that it updates with the amount of barnBucks the user has. Not sure if this should be in the BuyView class or the Farm
+    public static void updateBarnBucks(FarmLabel label, int amount) {
+        label.setText("                   Barn Bucks: " + amount);
+    }
+
+    private static JPanel createItemPanel(String itemName, String price, int maxQuantity, String description) {
+        // Item Label
+        JLabel itemLabel = new JLabel(itemName);
+
+        // Price Label
+        JLabel priceLabel = new JLabel("price: " + price);
+
+        // Quantity Label
+        JLabel quantityLabel = new JLabel("0");
+        quantityLabel.setPreferredSize(new Dimension(30, 20));
+        quantityLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        // Item Description Label
+        JLabel descriptionLabel = new JLabel(description);
+
+        // Buttons
+        JButton minusButton = new JButton("-");
+        JButton plusButton = new JButton("+");
+
+        // ActionListener for minus button
+        minusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int currentQuantity = Integer.parseInt(quantityLabel.getText());
+                if (currentQuantity > 0) {
+                    quantityLabel.setText(String.valueOf(currentQuantity - 1));
+                }
+            }
+        });
+
+        // ActionListener for plus button
+        plusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int currentQuantity = Integer.parseInt(quantityLabel.getText());
+                if (currentQuantity < maxQuantity) {
+                    quantityLabel.setText(String.valueOf(currentQuantity + 1));
+                }
+            }
+        });
+
+        // Item Panel
+        JPanel itemPanel = new JPanel(new GridBagLayout());
+        itemPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        itemPanel.setBackground(Color.WHITE);
+        itemPanel.setBorder(BorderFactory.createEmptyBorder(10, 150, 0, 200));
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        // Add Item Name
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        itemPanel.add(new JLabel(itemName), gbc);
+
+        // Add Price
+        gbc.gridx = 1;
+        itemPanel.add(new JLabel("Price: " + price), gbc);
+
+        // Add Minus Button
+        gbc.gridx = 2;
+        itemPanel.add(minusButton, gbc);
+
+        // Add Quantity Label
+        gbc.gridx = 3;
+        quantityLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        itemPanel.add(quantityLabel, gbc);
+
+        // Add Plus Button
+        gbc.gridx = 4;
+        itemPanel.add(plusButton, gbc);
+
+        // Add Description
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 5;
+        itemPanel.add(new JLabel(description), gbc);
+
+        return itemPanel;
     }
 }
