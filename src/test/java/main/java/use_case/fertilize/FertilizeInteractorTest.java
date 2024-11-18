c1bb418c98ccbbb75da7932fa3603d08f1a26fe4
mxiimy <momelody33@gmail.com>
2024-11-18 18:33:06 -0500

    Added file for fertilize test but since the use case doesn't do anything yet... no tests have been written yet

---
+++ src/test/java/main/java/use_case/fertilize/FertilizeInteractorTest.java
@@ -0,0 +1,15 @@
+package main.java.use_case.fertilize;
+
+import junit.framework.TestCase;
+import main.java.entity.Farm;
+import main.java.use_case.claim.ClaimInputBoundary;
+import main.java.use_case.claim.ClaimInteractor;
+import main.java.use_case.claim.ClaimOutputBoundary;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class FertilizeInteractorTest extends TestCase {
+
+}
