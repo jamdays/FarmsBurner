05f1fc8c91784af34c92f81a867c1e7cc0b1a8f8
mxiimy <momelody33@gmail.com>
2024-11-18 22:57:41 -0500

    Removed sprinkler tests. They should be uncommented once we implement a way to get tools more easily

--- src/test/java/main/java/entity/FarmTest.java
+++ src/test/java/main/java/entity/FarmTest.java
@@ -10,13 +10,15 @@ public class FarmTest extends TestCase {
         assertEquals(farmLand, farm.getFarmLand());
         assertEquals(0, farm.getBarnBucks());
         assertEquals(0, farm.getPower());
-        assertNull(farm.getSprinkler());
         farm.setPower(1);
         assertEquals(1, farm.getPower());
         farm.setBarnBucks(1);
         assertEquals(1, farm.getBarnBucks());
+        // TODO: uncomment this once we are able to start with no sprinkler
+        // assertNull(farm.getSprinkler());
         // TODO: uncomment this once we are able to get a sprinkler
-        assertNotNull(farm.getSprinkler());
+        // assertNotNull(farm.getSprinkler());
+        // TODO: add test cases for fertilize (once we figure out what it does)
     }
 
 }
