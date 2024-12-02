81d16dc152fe70f73d8e8cc4bc46e84af0c94627
jamdays <venuedollar@protonmail.com>
2024-12-02 17:07:50 -0500

    fixed some random stuff and started adding functionality in farmstate

--- src/main/java/interface_adapter/farm/FarmState.java
+++ src/main/java/interface_adapter/farm/FarmState.java
@@ -1,9 +1,12 @@
 package main.java.interface_adapter.farm;
 
+import main.java.interface_adapter.toolmenu.UpgradePresenter;
+
 public class FarmState {
     private int[][] farmLand;
     private long[][] cropTimes;
     private int[][] cropAges;
+    private int[][] prices;
     private final int WET = 0B1;
     private final int CLAIMED = 0B10;
     private final int SNOWY = 0B100;
@@ -19,6 +22,7 @@ public class FarmState {
     // 0 is night, 1 is day, 2 is sunrise/sunset
     private int day;
     private String weather;
+    private int temp;
     private String currTool;
     private int crop;
 
@@ -109,6 +113,186 @@ public class FarmState {
             this.crop = CORN;
         }
     }
+
+    /**
+     * Updates time for one crop
+     * private method (only called in updateAll)
+     * @param row row of plant to be updated
+     * @param col column of plant to be updated
+     * @param time new time
+     */
+    private void updateTime(int row, int col, long time){
+        if ((farmLand[row][col] & ALIVE) == ALIVE){
+            return;
+        }
+        if ((farmLand[row][col] & PLANTED) != PLANTED) {
+            return;
+        }
+        long diff = time - cropTimes[row][col];
+        long days = diff/86400;
+        if ((farmLand[row][col] & CROP_MASK) == SNOWBERRY){
+            updateSnowberry(row, col, days);
+        }
+        if ((farmLand[row][col] & CROP_MASK) == CORN){
+            updateCorn(row, col, days);
+        }
+        if ((farmLand[row][col] & CROP_MASK) == RICE){
+            updateRice(row, col, days);
+        }
+        if ((farmLand[row][col] & CROP_MASK) == WHEAT){
+            updateWheat(row, col, days);
+        }
+    }
+
+
+    /**
+     * Changes the age and updates the price with it
+     * <p>Assumes crop is alive</p>
+     * <p>Assumes crop exits at r c</p>
+     * The age could be set to negative
+     * Works best if age is only changed by 1 at a time
+     * @param row row where the crop is
+     * @param col column where the crop is
+     * @param age new age of the plant
+     */
+    private void changeAge(int row, int col, int age){
+        cropAges[row][col] = age;
+        if (age == 3){
+            prices[row][col] = 5;
+            if ((farmLand[row][col] & FERTILIZED) == FERTILIZED) {
+                prices[row][col] = prices[row][col] * 2;
+            }
+
+        }
+        if (age < 3){
+            prices[row][col] = 0;
+        }
+        if (age > 3 && age < 6){
+            prices[row][col] = prices[row][col] + 1;
+        }
+        if (age > 7){
+            prices[row][col] = prices[row][col] - 1;
+        }
+        if (prices[row][col] == 0){
+            farmLand[row][col] = farmLand[row][col] ^ ALIVE;
+        }
+    }
+
+    /**
+     * Updates the snowberry
+     * Private because it is only called in updateTime
+     * @param row row of plant
+     * @param col column of plant
+     * @param days days since planted
+     */
+    private void updateSnowberry(int row, int col, long days){
+        if (days >= 1 && (farmLand[row][col] & WET) == WET) {
+            farmLand[row][col] = farmLand[row][col] ^ WET;
+            changeAge(row, col, cropAges[row][col]);
+            if (temp < 3) {
+                if (this.getWeather().equalsIgnoreCase("Snow")) {
+                    changeAge(row, col, cropAges[row][col] + 1);
+                }
+                if (temp < -9){
+                    changeAge(row, col, cropAges[row][col] + 1);
+                }
+            }
+            else if (temp > 8 && temp < 19){
+                changeAge(row, col, cropAges[row][col] - 1);
+            }
+            else if (temp > 18){
+                farmLand[row][col] = farmLand[row][col] ^ ALIVE;
+            }
+        }
+    }
+
+
+    /**
+     * Updates the rice
+     * Private because it is only called in updateTime
+     * @param row row of plant
+     * @param col column of plant
+     * @param days days since planted
+     */
+    private void updateRice(int row, int col, long days){
+        if (days >= 1 && (farmLand[row][col] & WET) == WET) {
+            farmLand[row][col] = farmLand[row][col] ^ WET;
+            changeAge(row, col, cropAges[row][col]);
+            if (temp < 3) {
+                if (this.getWeather().equalsIgnoreCase("Snow")) {
+                    changeAge(row, col, cropAges[row][col] + 1);
+                }
+                if (temp < -9){
+                    changeAge(row, col, cropAges[row][col] + 1);
+                }
+            }
+            else if (temp > 8 && temp < 19){
+                changeAge(row, col, cropAges[row][col] - 1);
+            }
+            else if (temp > 18){
+                farmLand[row][col] = farmLand[row][col] ^ ALIVE;
+            }
+        }
+    }
+
+
+    /**
+     * Updates the corn
+     * Private because it is only called in updateTime
+     * @param row row of plant
+     * @param col column of plant
+     * @param days days since planted
+     */
+    private void updateCorn(int row, int col, long days){
+        if (days >= 1 && (farmLand[row][col] & WET) == WET) {
+            farmLand[row][col] = farmLand[row][col] ^ WET;
+            changeAge(row, col, cropAges[row][col]);
+            if (temp < 3) {
+                if (this.getWeather().equalsIgnoreCase("Snow")) {
+                    changeAge(row, col, cropAges[row][col] + 1);
+                }
+                if (temp < -9){
+                    changeAge(row, col, cropAges[row][col] + 1);
+                }
+            }
+            else if (temp > 8 && temp < 19){
+                changeAge(row, col, cropAges[row][col] - 1);
+            }
+            else if (temp > 18){
+                farmLand[row][col] = farmLand[row][col] ^ ALIVE;
+            }
+        }
+    }
+
+
+    /**
+     * Updates the wheat
+     * Private because it is only called in updateTime
+     * @param row row of plant
+     * @param col column of plant
+     * @param days days since planted
+     */
+    private void updateWheat(int row, int col, long days){
+        if (days >= 1 && (farmLand[row][col] & WET) == WET) {
+            farmLand[row][col] = farmLand[row][col] ^ WET;
+            changeAge(row, col, cropAges[row][col]);
+            if (temp < 3) {
+                if (this.getWeather().equalsIgnoreCase("Snow")) {
+                    changeAge(row, col, cropAges[row][col] + 1);
+                }
+                if (temp < -9){
+                    changeAge(row, col, cropAges[row][col] + 1);
+                }
+            }
+            else if (temp > 8 && temp < 19){
+                changeAge(row, col, cropAges[row][col] - 1);
+            }
+            else if (temp > 18){
+                farmLand[row][col] = farmLand[row][col] ^ ALIVE;
+            }
+        }
+    }
+
 
     public int getCrop(){
         return crop;
