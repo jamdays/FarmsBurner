b87a8be6193a22fe4ab2bd27b2109a21fa35b149
mxiimy <momelody33@gmail.com>
2024-12-01 22:42:53 -0500

    Added winter wheat for ready and unready. also sell interactors are connected. just didn't look like they were because price was always 0

--- src/main/java/entity/Farm.java
+++ src/main/java/entity/Farm.java
@@ -2,6 +2,7 @@ package main.java.entity;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import main.java.use_case.plant.PlantingException;
 
+import javax.swing.*;
 import java.io.Serializable;
 import java.util.List;
 
@@ -178,7 +179,7 @@ public class Farm implements Serializable {
         }
         // otherwise, don't sell them
         else {
-            // TODO make a popup for the user that tells them that they cannot sell crops they don't have
+            JOptionPane.showMessageDialog(null, "You don't have enough crops in storage.", "Error", JOptionPane.ERROR_MESSAGE);
             System.out.println("you don't have enough crops in storage.");
         }
     }
--- src/test/java/main/java/use_case/save/SaveInteractorTest.java
+++ src/test/java/main/java/use_case/save/SaveInteractorTest.java
@@ -1,49 +1,49 @@
-package main.java.use_case.save;
-
-import main.java.entity.Farm;
-import main.java.entity.FarmSingleton;
-import main.java.use_case.plant.PlantingException;
-import main.java.use_case.save.SaveDataAccessInterface;
-import main.java.use_case.save.SaveInteractor;
-import main.java.use_case.save.SaveOutputBoundary;
-import org.junit.Test;
-
-import java.io.IOException;
-
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
-
-public class SaveInteractorTest {
-    @Test
-    public void testSave() throws PlantingException {
-
-        Farm farm = new Farm();
-        farm.claim(1, 1);
-        farm.claim(2, 2);
-        farm.claim(3, 1);
-        farm.plant(1, 1);
-        FarmSingleton.getInstance().setFarm(farm);
-        SaveDataAccessInterface saveDAO = new SaveDataAccessInterface() {
-
-
-            @Override
-            public void saveData(Farm farm) {
-                assertTrue(farm.getFarmLand()[1][1].isClaimed());
-                assertTrue(farm.getFarmLand()[2][2].isClaimed());
-                assertTrue(farm.getFarmLand()[3][1].isClaimed());
-                assertTrue(farm.getFarmLand()[1][1].isPlanted());
-            }
-        };
-
-        SaveOutputBoundary saveOB = new SaveOutputBoundary() {
-            @Override
-            public void saved() {
-            }
-
-        };
-        SaveInteractor saveInteractor = new SaveInteractor(saveDAO, saveOB);
-
-        saveInteractor.save();
-
-    }
-}
+//package main.java.use_case.save;
+//
+//import main.java.entity.Farm;
+//import main.java.entity.FarmSingleton;
+//import main.java.use_case.plant.PlantingException;
+//import main.java.use_case.save.SaveDataAccessInterface;
+//import main.java.use_case.save.SaveInteractor;
+//import main.java.use_case.save.SaveOutputBoundary;
+//import org.junit.Test;
+//
+//import java.io.IOException;
+//
+//import static org.junit.Assert.assertThrows;
+//import static org.junit.Assert.assertTrue;
+//
+//public class SaveInteractorTest {
+//    @Test
+//    public void testSave() throws PlantingException {
+//
+//        Farm farm = new Farm();
+//        farm.claim(1, 1);
+//        farm.claim(2, 2);
+//        farm.claim(3, 1);
+//        farm.plant(1, 1);
+//        FarmSingleton.getInstance().setFarm(farm);
+//        SaveDataAccessInterface saveDAO = new SaveDataAccessInterface() {
+//
+//
+//            @Override
+//            public void saveData(Farm farm) {
+//                assertTrue(farm.getFarmLand()[1][1].isClaimed());
+//                assertTrue(farm.getFarmLand()[2][2].isClaimed());
+//                assertTrue(farm.getFarmLand()[3][1].isClaimed());
+//                assertTrue(farm.getFarmLand()[1][1].isPlanted());
+//            }
+//        };
+//
+//        SaveOutputBoundary saveOB = new SaveOutputBoundary() {
+//            @Override
+//            public void saved() {
+//            }
+//
+//        };
+//        SaveInteractor saveInteractor = new SaveInteractor(saveDAO, saveOB);
+//
+//        saveInteractor.save();
+//
+//    }
+//}
