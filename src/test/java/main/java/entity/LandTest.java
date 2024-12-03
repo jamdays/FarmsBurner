6365bdde62a4eff9817811b708beac3316820a73
mxiimy <momelody33@gmail.com>
2024-12-03 13:58:56 -0500

    Land and Storage tests

--- src/test/java/main/java/entity/LandTest.java
+++ src/test/java/main/java/entity/LandTest.java
@@ -5,59 +5,59 @@ import main.java.use_case.plant.PlantingException;
 
 public class LandTest extends TestCase {
 
-    /*
+
     public void testLand() throws PlantingException {
-        Land land = new Land();
+        Farm farm = new Farm();
+        farm.setWeather(0, false, false, false, false, false, true, 15, "sunny", System.currentTimeMillis());
+        farm.setBarnBucks(5000);
+        farm.setPower(5000);
+        FarmSingleton.getInstance().setFarm(farm);
 
         // isClaimed tests
-        assertFalse(land.isClaimed());
-        land.setClaimed(true);
-        assertTrue(land.isClaimed());
+        assertFalse(farm.getFarmLand()[0][0].isClaimed());
+        farm.getFarmLand()[0][0].water();
+        farm.claim(0, 0);
+        assertTrue(farm.getFarmLand()[0][0].isClaimed());
 
         // isPlanted tests
-        assertFalse(land.isPlanted());
-//        land.plant();
-        assertTrue(land.isPlanted());
-        assertNotNull(land.getCrop());
+        assertFalse(farm.getFarmLand()[0][0].isPlanted());
+        farm.getFarmLand()[0][0].water();
+        farm.getFarmLand()[0][0].plant(System.currentTimeMillis(), "wheat");
+        assertTrue(farm.getFarmLand()[0][0].isPlanted());
+        assertNotNull(farm.getFarmLand()[0][0].getCrop());
+        farm.getFarmLand()[1][1].setPlanted(false);
+        assertFalse(farm.getFarmLand()[1][1].isPlanted());
 
         // isWet tests
-        assertFalse(land.isWet());
-        land.water();
-        assertTrue(land.isWet());
-        land.setIsWet(false);
-        assertFalse(land.isWet());
+        assertFalse(farm.getFarmLand()[0][0].isWet());
+        farm.getFarmLand()[0][0].water();
+        assertTrue(farm.getFarmLand()[0][0].isWet());
+        farm.getFarmLand()[0][0].setIsWet(false);
+        assertFalse(farm.getFarmLand()[0][0].isWet());
 
         // isFertilized tests
-        land.setIsSnowy(false);
-        land.fertilize();
-        assertTrue(land.isFertilized());
-        land.setFertilized(false);
-        assertFalse(land.isFertilized());
+        farm.getFarmLand()[0][0].setIsSnowy(false);
+        farm.getFarmLand()[0][0].fertilize();
+        assertTrue(farm.getFarmLand()[0][0].isFertilized());
+        farm.getFarmLand()[0][0].setFertilized(false);
+        assertFalse(farm.getFarmLand()[0][0].isFertilized());
 
         // isSnowy tests
-        assertFalse(land.getIsSnowy());
-        land.setIsSnowy(true);
-        assertTrue(land.getIsSnowy());
+        assertFalse(farm.getFarmLand()[0][0].getIsSnowy());
+        farm.getFarmLand()[0][0].setIsSnowy(true);
+        farm.getFarmLand()[0][0].water();
+        assertTrue(farm.getFarmLand()[0][0].getIsSnowy());
 
         // harvest tests
-        land.setClaimed(true);
-        land.setIsSnowy(false);
-        land.getCrop().setAge(5);
-        assertTrue(land.isPlanted());
-        assertTrue(land.getCrop().getIsAlive());
-        land.harvest();
-        assertFalse(land.isPlanted());
-        assertNull(land.getCrop());
-
-        // Land with parameters tests
-//        Land land2 = new Land(new AbstractCrop());
-//        assertTrue(land2.isPlanted());
-//        assertTrue(land2.isClaimed());
-//        assertTrue(land2.isFertilized());
-//        assertFalse(land2.isWet());
-//        assertFalse(land2.getIsSnowy());
-
+        farm.getFarmLand()[0][0].setClaimed(true);
+        farm.getFarmLand()[0][0].setIsSnowy(false);
+        farm.getFarmLand()[0][0].getCrop().setAge(5);
+        assertTrue(farm.getFarmLand()[0][0].isPlanted());
+        assertTrue(farm.getFarmLand()[0][0].getCrop().getIsAlive());
+        farm.getFarmLand()[0][0].harvest();
+        assertFalse(farm.getFarmLand()[0][0].isPlanted());
+        assertNull(farm.getFarmLand()[0][0].getCrop());
     }
-     */
+
 
 }
