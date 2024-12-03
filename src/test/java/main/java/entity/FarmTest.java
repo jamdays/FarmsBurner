2e9dd247bb9f4878b3cd55d39fd0a0fb5093622e
mxiimy <momelody33@gmail.com>
2024-12-03 14:48:25 -0500

    farm is FINALLY 100

--- src/test/java/main/java/entity/FarmTest.java
+++ src/test/java/main/java/entity/FarmTest.java
@@ -1,24 +1,94 @@
 package main.java.entity;
 
 import junit.framework.TestCase;
+import main.java.use_case.plant.PlantingException;
 
 public class FarmTest extends TestCase {
 
-    public void testFarm() {
-        Land[][] farmLand = new Land[8][10];
-        Farm farm = new Farm(farmLand);
-        assertEquals(farmLand, farm.getFarmLand());
+    public void testFarm() throws PlantingException {
+        Farm farm = new Farm();
+        FarmSingleton.getInstance().setFarm(farm);
+        farm.setWeather(0, false, false, false, false, false, true, 15, "sunny", 0L);
+        farm.getFarmLand();
         assertEquals(0, farm.getBarnBucks());
-        assertEquals(0, farm.getPower());
         farm.setPower(1);
         assertEquals(1, farm.getPower());
         farm.setBarnBucks(1);
         assertEquals(1, farm.getBarnBucks());
-        // TODO: uncomment this once we are able to start with no sprinkler
-        // assertNull(farm.getSprinkler());
-        // TODO: uncomment this once we are able to get a sprinkler
-        // assertNotNull(farm.getSprinkler());
-        // TODO: add test cases for fertilize (once we figure out what it does)
+        farm.setSprinklerPurchased(true);
+        farm.setBarnBucks(100000);
+
+        farm.setCity("Toronto");
+        assertEquals("Toronto", farm.getCity());
+
+        farm.setSprinklerPurchased(true);
+        assertTrue(farm.getSprinklerPurchased());
+        farm.setSprinklerLevel(1);
+        assertEquals(1, farm.getSprinklerLevel());
+
+        farm.setHarvesterPurchased(true);
+        assertTrue(farm.getHarvesterPurchased());
+        farm.setHarvesterLevel(1);
+        assertEquals(1, farm.getHarvesterLevel());
+
+        farm.setTillerPurchased(true);
+        assertTrue(farm.getTillerPurchased());
+        farm.setTillerLevel(1);
+        assertEquals(1, farm.getTillerLevel());
+
+        farm.setFertilizerPurchased(true);
+        assertTrue(farm.getFertilizerPurchased());
+        farm.setFertilizerLevel(1);
+        assertEquals(1, farm.getFertilizerLevel());
+
+        farm.setPlanterPurchased(true);
+        assertTrue(farm.getPlanterPurchased());
+        farm.setPlanterLevel(1);
+        assertEquals(1, farm.getPlanterLevel());
+
+        farm.setActiveTool("sprinkler");
+        assertEquals("sprinkler", farm.getActiveTool());
+
+        farm.setActiveCrop("wheat");
+        assertEquals("wheat", farm.getActiveCrop());
+
+        farm.claim(0,0);
+        farm.fertilize(0,0);
+        farm.plant(0,0, System.currentTimeMillis());
+        farm.water(0,0);
+        farm.harvest(0,0);
+        farm.sell(1);
+
+        farm.setLogOutTime(0L);
+        farm.getTime();
+
+        Storage storage = new Storage(1);
+        farm.setStorage(storage);
+        assertEquals(1, farm.getStorage().getCapacity());
+
+        farm.isClear();
+        farm.isCloudy();
+        farm.isFog();
+        farm.isThunderstorm();
+        farm.isSnowy();
+        farm.isRainy();
+        farm.getDay();
+
+        farm.setTemp(1);
+        assertEquals(1, farm.getTemp());
+
+        farm.setWeather(1, true, true, true, true, true, true, 1, "rainy", 1);
+        farm.getWeather();
+        farm.weather("sunny");
+    }
+
+    public void testRefreshPower(){
+        Land[][] farmLand = new Land[8][10];
+        Farm farm = new Farm(farmLand);
+        farm.setPower(1);
+        farm.refreshPower(System.currentTimeMillis());
+        farm.getPowerRefresh();
+        assertTrue(true);
     }
 
 }
